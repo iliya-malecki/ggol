ebaa5b768e4eee5f5b313a0600dc0b3df96422c8
Iliya Malecki <iliyamalecki@gmail.com>
2022-09-26 02:28:09 +0300

    created the game

---
+++ ggol.py
@@ -0,0 +1,97 @@
+from typing import Protocol
+import cv2
+import numpy as np
+import pygame
+import good_rules
+
+class GOLRuleset(Protocol):
+    def __call__(self, field: np.ndarray) -> np.ndarray:
+        ...
+
+    def intervention(self, field:np.ndarray, x: int, y: int) -> None:
+        ...
+
+
+class Buffer:
+
+    def __init__(self, color, size):
+        self.size = size
+        self._buffer = []
+        self.color = np.array(color)
+
+    def append(self, field):
+        self._buffer.append(field)
+        if len(self._buffer) > self.size:
+            self._buffer.pop(0)
+    
+    def get_smoothed(self):
+        return (
+            (np.mean(np.stack(self._buffer), axis=0))[..., np.newaxis] 
+            * self.color
+        ).astype('uint8')
+        
+
+class Viewer:
+
+    def __init__(self, field: np.ndarray, rules: GOLRuleset, display_size:tuple[int, int], color, buffer_size):
+        pygame.init()
+        self.field = field
+        self.rules = rules
+        self.display = pygame.display.set_mode(display_size)
+        self.font = pygame.font.SysFont("Arial", 18)
+        self.clock = pygame.time.Clock()
+        self.buffer = Buffer(color, buffer_size)
+        
+    def set_title(self, title):
+        pygame.display.set_caption(title)
+    
+    def start(self):
+        textcolor = pygame.Color("coral")
+        alive = True
+        paused = False
+        while alive:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    alive = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        paused = ~paused
+                elif event.type == pygame.MOUSEMOTION:
+                    if pygame.mouse.get_pressed()[0]:
+                        posx, posy = pygame.mouse.get_pos()
+                        disx, disy = self.display.get_size()
+                        fieldx, fieldy = self.field.shape
+                        x = int(posx/disx*fieldx)
+                        y = int(posy/disy*fieldy)
+                        try:
+                            self.rules.intervention(self.field, x, y)
+                        except (ValueError, IndexError):
+                            pass
+
+            if not paused:
+
+                self.field = self.rules(self.field)
+                
+                self.buffer.append(self.field)
+
+                surf = pygame.transform.scale(
+                            pygame.surfarray.make_surface(
+                                self.buffer.get_smoothed()),
+                            self.display.get_size()
+                        )
+                self.display.blit(surf, (0, 0))
+
+                self.clock.tick(70)
+                fps = str(self.clock.get_fps())
+                self.display.blit(self.font.render(fps, 1, textcolor), (0, 0))
+
+            pygame.display.update()
+
+        pygame.quit()
+
+
+
+rules = good_rules.moving_rocks
+field = np.random.binomial(1,p=rules.initialization_percentage, size=(400,400)).astype('float32')
+viewer = Viewer(field, rules, (800, 800), (60,20,10), 4)
+viewer.start()
---
+++ good_rules.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass
+import numpy as np
+import cv2
+def function():
+    ...
+function = type(function)
+
+def basic_convolution(field: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    return cv2.filter2D(field, -1, kernel, borderType=cv2.BORDER_ISOLATED)
+
+def inv_gaussian_activation(field: np.ndarray) -> np.ndarray:
+    return (-1./np.power(2., (0.6*np.power(field, 2.)))+1.)
+
+def basic_intervention(field: np.ndarray, x: int, y:int) -> None:
+    field[x, y] = 1
+
+def checkerboard_intervetion(field: np.ndarray, x: int, y:int) -> None:
+    field[x-5:x+5, y-5:y+5] = (
+        np.array([[1,0]*5, [0,1]*5] * 5)
+        * cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
+    )
+
+
+@dataclass
+class CallableRuleset:
+
+    kernel: np.ndarray
+    convolution: function
+    activation: function
+    intervention: function
+    initialization_percentage: float = 0.5
+
+    def __call__(self, field: np.ndarray) -> np.ndarray:
+        return self.activation(self.convolution(field, self.kernel))
+
+
+
+classic = CallableRuleset(
+    kernel = np.array([
+        [1,1,1],
+        [1,9,1],
+        [1,1,1],
+    ], dtype='uint8'),
+    convolution = basic_convolution,
+    activation = lambda field: ((field == 3) | (field == 11) | (field == 12)),
+    intervention = basic_intervention
+)
+
+slime_pulling_worms = CallableRuleset(
+    kernel = np.array([
+        [  0.74, -0.946,   0.74],
+        [-0.946, -0.434, -0.946],
+        [  0.74, -0.946,   0.74],
+    ], dtype='float32'),
+    convolution = basic_convolution,
+    activation = inv_gaussian_activation,
+    intervention = checkerboard_intervetion
+)
+
+blood_pumping_worms = CallableRuleset(
+    kernel = np.array([
+        [ 0.742, -0.966, 0.742],
+        [-0.966, -0.45, -0.966],
+        [ 0.742, -0.966, 0.742],
+    ], dtype='float32'),
+    convolution = basic_convolution,
+    activation = inv_gaussian_activation,
+    intervention = checkerboard_intervetion
+)
+
+pipes = CallableRuleset(
+    kernel = np.array([
+        [-.2,  -0.1,     0,    .1,   -.2],
+        [ .1,     0,   .55,    .0,   -.1],
+        [  0,   .55,   1.3,   .55,     0],
+        [-.1,     0,   .55,    .0,    .1],
+        [-.2,    .1,     0,   -.1,   -.2],
+    ], dtype='float32'),
+    convolution = basic_convolution,
+    activation = inv_gaussian_activation,
+    intervention = checkerboard_intervetion,
+    initialization_percentage = 0.1
+)
+
+
+moving_rocks = CallableRuleset(
+    kernel = np.array([
+        [-.3,   -.3,    .1,    .3,   -.3],
+        [ .3,    .0,   .55,    .0,   -.3],
+        [ .1,   .55,   1.2,   .55,    .1],
+        [-.3,    .0,   .55,    .0,    .3],
+        [-.6,    .3,    .1,   -.3,   -.3],
+    ], dtype='float32'),
+    convolution = basic_convolution,
+    activation = inv_gaussian_activation,
+    intervention = checkerboard_intervetion,
+    initialization_percentage = 0.2
+)
