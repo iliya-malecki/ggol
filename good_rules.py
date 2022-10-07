b34e1371f0bfcc72342e7cbc0b77664a6cbce013
Iliya Malecki <iliyamalecki@gmail.com>
2022-10-07 11:44:48 +0200

    more housekeeping

--- good_rules.py
+++ good_rules.py
@@ -4,7 +4,6 @@ import cv2
 import numba 
 from types import FunctionType
 
-
 jit = numba.jit(nopython=True, parallel=True, fastmath=True)
 
 
--- with_gl.py
+++ with_gl.py
@@ -3,11 +3,14 @@ import good_rules
 from glumpy import app, gloo, gl, data
 
 app.use('qt5')
+
 rules = good_rules.blood_pumping_worms
 window = app.Window(fullscreen=True)
 h, w = window.get_size()
-frac = 1/4
-field = np.random.binomial(1,p=rules.initialization_percentage, size=(int(w*frac), int(h*frac))).astype('float32')
+field_size = 1/2
+if isinstance(field_size, float):
+    field_size = (int(w*field_size), int(h*field_size))
+field = np.random.binomial(1,p=rules.initialization_percentage, size=field_size).astype('float32')
 quad = gloo.Program(
     vertex="""
         attribute vec2 position;
@@ -20,18 +23,19 @@ quad = gloo.Program(
         }
     """, 
     fragment="""
+        uniform vec3 color;
         uniform sampler2D texture;
         varying vec2 v_texcoord;
         void main()
         {
-            gl_FragColor = vec4(vec3(.4,1,0) * texture2D(texture, v_texcoord).r, 1);
+            gl_FragColor = vec4(color * texture2D(texture, v_texcoord).r, 1);
         }
     """,
     count=4)
 quad['position'] = [(-1,-1), (-1,+1), (+1,-1), (+1,+1)]
 quad['texcoord'] = [( 0, 1), ( 0, 0), ( 1, 1), ( 1, 0)]
 quad['texture'] = field
-
+quad['color'] = np.array([200,10,150]) / 255
 
 
 @window.event
@@ -39,11 +43,8 @@ def on_draw(dt):
     global field
     field = rules(field)
     quad['texture'] = field
-    window.clear()
+    # window.clear()
     quad.draw(gl.GL_TRIANGLE_STRIP)
     print(window.fps)
 
 app.run(framerate=500)
-# print(window.get_size())
-# print(field)
-gloo.FrameBuffer
